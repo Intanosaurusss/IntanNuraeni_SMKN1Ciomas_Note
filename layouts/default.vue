b27fa508c55e171277dbaa34b6d683dc4f1e2dd8
intanosaurusss <aeni6746@gmail.com>
2023-12-04 18:36:54 +0700

    mengupload tugas akhir smkcoding

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <Nuxt-link to="/"> Create Notes</Nuxt-link>
+        </li>
+        <li>
+          <Nuxt-link to="/Notes"> My Notes</Nuxt-link>
+        </li>
+      </ul>
+    </nav>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+nav {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.589);
+  overflow: hidden; /* Hide overflowing content */
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  transition: transform 0.3s ease; /* Add smooth transition for the list items */
+}
+
+nav ul li {
+  margin: 0 10px;
+  transform: translateY(0); /* Initial position for the list items */
+  transition: transform 0.3s ease; /* Add smooth transition for the list items */
+}
+
+nav ul li:hover {
+  transform: translateY(-5px); /* Move the list item up on hover */
+}
+
+nav ul li a {
+  text-decoration: none;
+  color: #fff;
+  padding: 10px;
+  border: 1px solid #fff;
+  transition: background 0.3s ease; /* Add smooth transition for the background */
+}
+
+nav ul li a:hover {
+  background: #000;
+}
+
+.nuxt-link-exact-active {
+  background: #000;
+}
+</style>
---
+++ pages/Notes.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container-notes">
+    <div class="allnots" v-for="note in Notes" :key="note.id">
+      <header>
+        <h1>{{ note.title }}</h1>
+        <img
+          src="@/assets/image/remove.png"
+          alt=""
+          @click="remove(note.index)"
+        />
+      </header>
+      <section>
+        <p>{{ note.des }}</p>
+      </section>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: "Notes"
+    };
+  },
+  computed: {
+    Notes() {
+      return this.$store.getters.getnotes;
+    }
+  },
+  methods: {
+    remove(item) {
+      this.$store.commit("removenote", item);
+      console.log(item);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container-notes {
+ background: linear-gradient(135deg, #435970, #00bfff);
+ padding: 20px;
+ box-sizing: border-box;
+ width: 100%;
+ height: 100%;
+}
+</style>
